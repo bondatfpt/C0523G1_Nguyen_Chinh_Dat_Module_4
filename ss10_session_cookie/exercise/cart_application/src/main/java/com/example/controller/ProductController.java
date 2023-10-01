33121676540b11c88318636718adc4de343bf3b9
bondatfpt <bondatfpt@gmail.com>
2023-10-01 19:00:45 +0700

    ok

--- ss10_session_cookie/exercise/cart_application/src/main/java/com/example/controller/ProductController.java
+++ ss10_session_cookie/exercise/cart_application/src/main/java/com/example/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Controller
@@ -31,15 +32,24 @@ public class ProductController {
     @GetMapping("/details/{id}")
     public String showFormDetails(@PathVariable Integer id, Model model) {
         Product product = iProductService.findById(id);
-        model.addAttribute("productDetails", product);
+        if (product == null) {
+            model.addAttribute("error", "Sản phẩm không tồn tại !");
+        } else {
+            model.addAttribute("productDetails", product);
+        }
         return "form-details";
     }
 
     @GetMapping("/addToCart/{id}")
-    public String addToCart (@PathVariable Integer id, @SessionAttribute(value = "cart") CartDto cartDto){
+    public String addToCart(@PathVariable Integer id, @SessionAttribute(value = "cart") CartDto cartDto, Model model) {
         Product product = iProductService.findById(id);
-        cartDto.addProduct(product);
-        return "redirect:/carts";
+        if (product == null) {
+            model.addAttribute("error", "Sản phẩm không tồn tại !");
+            return "form-details";
+        } else {
+            cartDto.addProduct(product);
+            return "redirect:/carts";
+        }
     }
 
 }
